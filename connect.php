9506997495c796ad41a9ad93a14c2254642ac39b
Frankie Cancino <frankiecancino@gmail.com>
2015-12-16 20:23:22 -0600

    Update connect.php

--- connect.php
+++ connect.php
@@ -32,10 +32,4 @@ THE SOFTWARE.
                 header('Location: ' . $url);
         }
         
-        $host = "us-cdbr-azure-central-a.cloudapp.net";
-        $user = "bac7425156859d";
-        $pass = "ad2aa049";
-        $db = "logboatmysqldb";
-        $con = mysqli_connect($host, $user, $pass, $db) or die ("Connection Error" . mysqli_error($link)); // ToDo: Better failing system
-
 ?>
